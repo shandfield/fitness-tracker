b96d08a62f8630c42cff24d2a33aeab30b9b2b29
Shelby Handfield <shelbyhandfield@protonmail.com>
2020-08-13 19:53:03 -0600

    making a lot of code edits in this file to match up to my other calls/routes/functions

--- routes/api.js
+++ routes/api.js
@@ -3,32 +3,32 @@ const Workout = require("../models/workout");
 
 module.exports = function (app) {
     //this will create the db of Workout
-    app.post("/api/workouts", (req,res) =>{
+    app.post("/api/exercise", (req,res) =>{
         db.Workout.create({})
         .then(dbWorkout =>{res.json(dbWorkout)})
         .catch(err => {res.json(err)})
     })
 
     //this will post new exercise data into workout db
-    app.post("/api/workouts/:id", ({ body }, res)=>{
+    app.post("/api/exercise/:id", ({ body }, res)=>{
         db.Workout.findByIdAndUpdate(params.id, {$push: {exercises: body}},{new:true})    
         .then(dbWorkout => {res.json(dbWorkout)})
         .catch(err => {res.json(err)})
     });
     //this will find all workouts
-    app.get("/api/workouts", (req,res) => {
+    app.get("/api/exercise", (req,res) => {
         db.Workout.find({})
         .then(workout => {res.json(workout)})
         .catch(err => {res.json(err)})
     });
     //this is to find the date range for the workouts
-    app.get("/api/workouts/range", ({query}, res)=>{
+    app.get("/api/stats/range", ({query}, res)=>{
         Workout.find({ day: {$gte: query.start, $lte: query.end}})
         .then(dbWorkouts => {res.json(dbWorkouts)})
         .catch(err => res.json(err))
     });
     //this will delete selected workouts based on id
-    app. delete("/api/workouts",({body},res) =>{
+    app. delete("/api/exercise",({body},res) =>{
         Workout.findByIdAndDelete(body.id)
         .then(() => {res.json(true)})
         .catch(err => res.json(err))
