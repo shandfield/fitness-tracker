ba7e9996249eee51e0e29c1d0675f89a0d322690
Shelby Handfield <shelbyhandfield@protonmail.com>
2020-08-13 19:14:02 -0600

    created routes to create the db, add excercises, delete them and view them

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,36 @@
 const db = require("../models");
+const Workout = require("../models/workout");
 
 module.exports = function (app) {
-    
-}
+    //this will create the db of Workout
+    app.post("/api/workouts", (req,res) =>{
+        db.Workout.create({})
+        .then(dbWorkout =>{res.json(dbWorkout)})
+        .catch(err => {res.json(err)})
+    })
+
+    //this will post new exercise data into workout db
+    app.post("/api/workouts/:id", ({ body }, res)=>{
+        db.Workout.findByIdAndUpdate(params.id, {$push: {exercises: body}},{new:true})    
+        .then(dbWorkout => {res.json(dbWorkout)})
+        .catch(err => {res.json(err)})
+    });
+    //this will find all workouts
+    app.get("/api/workouts", (req,res) => {
+        db.Workout.find({})
+        .then(workout => {res.json(workout)})
+        .catch(err => {res.json(err)})
+    });
+    //this is to find the date range for the workouts
+    app.get("/api/workouts/range", ({query}, res)=>{
+        Workout.find({ day: {$gte: query.start, $lte: query.end}})
+        .then(dbWorkouts => {res.json(dbWorkouts)})
+        .catch(err => res.json(err))
+    });
+    //this will delete selected workouts based on id
+    app. delete("/api/workouts",({body},res) =>{
+        Workout.findByIdAndDelete(body.id)
+        .then(() => {res.json(true)})
+        .catch(err => res.json(err))
+    })
+};
